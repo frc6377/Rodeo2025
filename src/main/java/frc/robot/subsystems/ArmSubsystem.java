5d118acbd3ae1881fb97c919c39af908c2ea68bf
Jackson <79735432+ModBoyex@users.noreply.github.com>
2024-09-14 16:01:59 -0500

    Add last bits of arm so fully works

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,34 +17,47 @@ public class ArmSubsystem extends SubsystemBase {
   private final DutyCycleEncoder m_armEncoder;
   public boolean isStowed;
   private final ArmFeedforward feedforwardArm;
+  private final PIDController armPidController;
 
   public ArmSubsystem() {
     isStowed = true;
     m_armMotor = new TalonSRX(5);
     m_armEncoder = new DutyCycleEncoder(1);
+    m_armEncoder.reset();
+//TODO: add amout for proper offset so FF works.
     m_armEncoder.setDistancePerRotation(Math.PI * 2);
+    armPidController =
+        new PIDController(ArmConstants.kArmP, ArmConstants.kArmI, ArmConstants.kArmD);
     feedforwardArm =
         new ArmFeedforward(1.0, ArmConstants.kArmG, ArmConstants.kArmV, ArmConstants.kArmA);
   }
 
   public Command changeArmState() {
-    if (isStowed == true) {
-      isStowed = false;
-      return new PIDCommand(armPIDController
-          () -> {
-            setArmMotors(feedforwardArm.calculate(m_armEncoder.getAbsolutePosition(), 0, 0)/RobotController.getBatteryVoltage());
-          });
-    } else {
-      isStowed = true;
-      return run(
-          () -> {
-            setArmMotors(feedforwardArm.calculate(m_armEncoder.getAbsolutePosition(), 0, 0)/RobotController.getBatteryVoltage());
-          });
-    }
+
+    isStowed = !isStowed;
+    return new PIDCommand(
+        armPidController,
+        this::getArmAngle,
+        () -> {
+          if (!isStowed) {
+            return ArmConstants.scoreAngle;
+          } else {
+            return ArmConstants.stowedAngle;
+          }
+        },
+        (a) -> {
+          setArmMotors(
+              a
+                  + feedforwardArm.calculate(getArmAngle(), 0, 0)
+                      / RobotController.getBatteryVoltage());
+        },
+        this);
   }
-  private double getArmAngle () {
-    return m_armEncoder.getAbsolutePosition();
+
+  private double getArmAngle() {
+    return m_armEncoder.getDistance();
   }
+
   private void setArmMotors(double output) {
     m_armMotor.set(ControlMode.PercentOutput, output);
   }
